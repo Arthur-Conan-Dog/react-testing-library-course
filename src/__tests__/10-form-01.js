adae09df3d3cff71d6e57c84b6a82e95224d5626
Shaw <yrli@thoughtworks.com>
2020-07-24 16:59:47 +0800

    refactor 10-1

--- src/__tests__/10-form-01.js
+++ src/__tests__/10-form-01.js
@@ -2,22 +2,26 @@
 // import { render, fireEvent, wait } from '@testing-library/react'
 // import UserAccountForm from '../user-account-form'
 
-// note that you may encounter some unexpected problems like elements are not responding correctly while doing this exercise,
-// try find out the solutions from ant-design/testing-library repo issues or stackoverflow.
+// 🌟note that you may encounter some problems like elements are not responding as you expected while doing this exercise,
+// 🌟try find out the solutions on stackoverflow, or ant-design/testing-library repo issues.
+
+// 🤔after you've solved all those problems above and made all tests pass, what other problems your code might have introduced?
+// 🤔(say if our codebase has quite a size, and both ant-design and it grows rapidly
+
 test('should submit with correct values', () => {
   // 1. render UserAccountForm and provide a mock submit function
   // 2. change values of required fields
   // 3. find submit button and fire click/submit event
   // 4. make assertions that mock submit function has been called with correct values
-})
+});
 
 test('should show feedback messages when validation fails', () => {
-  // 1. render UserAccountForm and provide a mock submit function
-  // 2. find submit button and fire click/submit event
-  // 3. make assertions that messages are shown
-})
+  // 5. render UserAccountForm and provide a mock submit function
+  // 6. find submit button and fire click/submit event
+  // 7. make assertions that messages are shown
+});
 
 test('should be filled with user info when is editing', () => {
-  // 1. render UserAccountForm in edit mode
-  // 2. make assertions that all fields are filled with user info you provided above
-})
+  // 8. render UserAccountForm in edit mode
+  // 9. make assertions that all fields are filled with user info you provided above
+});
--- src/__tests__/form-01.final.js
+++ src/__tests__/form-01.final.js
@@ -1,53 +1,60 @@
-import React from 'react'
-import { render, fireEvent, wait } from '@testing-library/react'
-import { act } from 'react-dom/test-utils';
-import UserAccountForm from '../user-account-form'
+import React from 'react';
+import { render, fireEvent, wait } from '@testing-library/react';
+import UserAccountForm from '../user-account-form';
 
 test('should submit with correct values', async () => {
-  const submit = jest.fn()
-  const { container, getByLabelText, getByText } = render(<UserAccountForm submit={submit} />)
+  const submit = jest.fn();
+  const { getByLabelText, getByText } = render(
+    <UserAccountForm submit={submit} />
+  );
 
-  fireEvent.change(getByLabelText(/username/i), { target: { value: 'Jane' } })
-  fireEvent.change(getByLabelText(/password/i), { target: { value: 'pwd123' } })
+  fireEvent.change(getByLabelText(/username/i), { target: { value: 'Jane' } });
+  fireEvent.change(getByLabelText(/password/i), {
+    target: { value: 'pwd123' },
+  });
 
-  fireEvent.mouseDown(getByLabelText(/plan/i))
-  fireEvent.click(getByText(/free/i))
+  fireEvent.mouseDown(getByLabelText(/plan/i));
+  fireEvent.click(getByText(/free/i));
 
-  fireEvent.click(getByText(/create/i).parentElement)
+  fireEvent.click(getByText(/create/i).parentElement);
 
   await wait(() =>
     expect(submit).toHaveBeenCalledWith({
       username: 'Jane',
       password: 'pwd123',
       plan: 0,
-    }),
-  )
-})
+    })
+  );
+});
 
 test('should show error messages when validation fails', async () => {
-  const submit = jest.fn()
-  const { container, getByText } = render(<UserAccountForm submit={submit} />)
+  const submit = jest.fn();
+  const { getByText } = render(<UserAccountForm submit={submit} />);
 
-  fireEvent.click(getByText(/create/i).parentElement)
+  fireEvent.click(getByText(/create/i).parentElement);
 
   await wait(() => {
-    expect(getByText(/please input username/i)).not.toBeNull()
-    expect(getByText(/please input password/i)).not.toBeNull()
-    expect(getByText(/please choose a plan/i)).not.toBeNull()
-  })
-})
+    expect(getByText(/please input username/i)).not.toBeNull();
+    expect(getByText(/please input password/i)).not.toBeNull();
+    expect(getByText(/please choose a plan/i)).not.toBeNull();
+  });
+});
 
-test('should be filled with user info when is editing', async () => {
-  const submit = jest.fn()
+test('should be filled with user info when is editing', () => {
+  const submit = jest.fn();
   const user = {
     email: 'jane@gmail.com',
     password: 'pwd123',
     username: 'Jane Doe',
     plan: 1,
-  }
-  const { container, getByLabelText, getByText } = render(<UserAccountForm isEditing user={user} submit={submit} />)
+  };
+  const { getByLabelText } = render(
+    <UserAccountForm isEditing user={user} submit={submit} />
+  );
 
-  expect(getByLabelText(/username/i)).toHaveValue('Jane Doe')
-  expect(getByLabelText(/password/i)).toHaveValue('pwd123')
-  expect(getByLabelText(/plan/i).parentElement.parentElement).toHaveTextContent('Team')
-})
+  expect(getByLabelText(/username/i)).toHaveValue('Jane Doe');
+  expect(getByLabelText(/password/i)).toHaveValue('pwd123');
+  expect(getByLabelText(/plan/i).parentElement.parentElement).toHaveTextContent(
+    'Team'
+  );
+});
