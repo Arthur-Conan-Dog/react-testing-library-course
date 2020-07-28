df7597d45e753386de5c4afe2546c9b599ed28ef
Shaw <yrli@thoughtworks.com>
2020-07-28 12:12:42 +0800

    tweak

--- src/__tests__/01-react-dom.js
+++ src/__tests__/01-react-dom.js
@@ -1,10 +1,11 @@
+// 1. the follwing imports will be all you need in this exercise, uncomment to activate them!
 // import React from 'react'
 // import ReactDOM from 'react-dom'
 // import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
-  // create a div using DOM API as root for React component to be mounted
-  // render the FavoriteNumber component to that div using a React API
-  // find the input element using DOM API and assert the input type attribute is `number`
-  // find the label element using DOM API and assert its text content is "Favorite Number"
-})
+  // 2. create a div using DOM's createElement API as root for React component to be mounted
+  // 3. render the FavoriteNumber component to that div using ReactDOM.render.
+  // 4. find the input element using DOM's querySelector API and assert the input type attribute is `number`
+  // 5. find the label element using DOM's querySelector API and assert its text content is "Favorite Number"
+});
--- src/__tests__/02-jest-dom.js
+++ src/__tests__/02-jest-dom.js
@@ -1,10 +1,14 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import { FavoriteNumber } from '../favorite-number'
+// 🌟https://github.com/testing-library/jest-dom
+// 🌟Note that you won't be needing to import 'jest-dom' by yourself in this exercise, it's been already setup.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FavoriteNumber } from '../favorite-number';
 
 test('renders a number input with a label "Favorite Number"', () => {
-  // create a div using DOM API as root for React component to be mounted
-  // render the FavoriteNumber component to that div using a React API
-  // assert the input type attribute is `number` by using jest-dom's API
-  // assert the label text content is "Favorite Number" by using jest-dom's API
-})
+  const div = document.createElement('div');
+  ReactDOM.render(<FavoriteNumber />, div);
+  // assert the input type attribute is `number` by using jest-dom's toHaveAttribute API
+  expect(div.querySelector('input').type).toBe('number');
+  // assert the label text content is "Favorite Number" by using jest-dom's toHaveTextContent API
+  expect(div.querySelector('label').textContent).toBe('Favorite Number');
+});
