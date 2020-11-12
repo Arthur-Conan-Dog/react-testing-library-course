c75ca9653db5bca171b0b87d811594c03412e6f2
Shaw <yrli@thoughtworks.com>
2020-11-12 18:37:50 +0800

    update

--- src/__tests__/07-redux-03.js
+++ src/__tests__/07-redux-03.js
@@ -5,11 +5,12 @@
 // import { Counter } from '../redux-counter';
 // import { reducer } from '../redux-reducer';
 
-// implement this custom render function so that we could easily deal with all connected-like components, such as Redux, ReactRounter, Formik etc.
-// some requirements:
-// 1. user can provide initialState or the entire store that the ui is rendered with
-// 2. user can get all help utilities given by react-testing-library
-// 3. you could also return the store from this function, just note that you should avoid using store to test implementation details
+// implement this custom render function so that we could easily deal with all redux-connected components.
+// requirements:
+// 1. user can provide initialState or the entire store that the ui is rendered with;
+// 2. user can get all help utilities given by react-testing-library;
+// 3. be careful not to block the original features of the `render` function.
+// FYI: you could also return the store from this function, just note that you should avoid using store to test implementation details.
 
 // function render(ui) {
 //   const utils = rtlRender(ui);
